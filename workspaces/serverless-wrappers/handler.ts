86afc862d5cd8c7ebf7508ff44239836ebfb2fbb
dposse <dposse.125@gmail.com>
2020-04-08 18:57:47 -0400

    Add boilerplate handler for mlb service

--- workspaces/serverless-wrappers/handler.ts
+++ workspaces/serverless-wrappers/handler.ts
@@ -1,8 +1,15 @@
 import { Handler, ScheduledEvent, Context } from 'aws-lambda';
 import 'source-map-support/register';
 
+// in aws this lambda is named bball-slackbot-upgraded-[dev/prod]-runBot
 export const runBot: Handler = async (event: ScheduledEvent, _context: Context) => {
-  console.log(`let's see how this goes`);
+  console.log(`in runBot`);
+  console.log(`Event: \n${JSON.stringify(event, null, 2)}`);
+  return _context.logStreamName;
+}
+
+export const checkMLBGamesLambda: Handler = async (event, _context: Context) => {
+  console.log(`in checkMLBGamesLambda`);
   console.log(`Event: \n${JSON.stringify(event, null, 2)}`);
   return _context.logStreamName;
 }
