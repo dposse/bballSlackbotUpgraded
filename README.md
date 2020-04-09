f6b50b05d38d9ecadf3a574106f0697bd49c18d4
dposse <dposse.125@gmail.com>
2020-04-09 17:36:36 -0400

    runBot successfully invokes checkMLBGamesLambda

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-### bball-slackbot-upgraded
+## bball-slackbot-upgraded
 
 A simple troll bot that sends messages to a specified user on slack when their most hated baseball team wins.
 
 This project started as a simple node script we could run locally, but we wanted to learn how to properly setup AWS Lambdas.
+
+Does a project of this scale need lambdas? No. No it does not.
 
 ---
 
@@ -27,11 +29,19 @@ To check function logs, i.e. function runBot, run
 
     serverless logs -f runBot
 
+Run the function locally with
+
+    serverless invoke local -f runBot
+
+as well as running the function on AWS
+
+    serverless invoke -f runBot
+
 ### Random notes
 
 All `serverless` commands can be run with `sls`
 
-The initial setup for this project ran `serverless create` in the root directory, which created and deployed successfully. We decided to use yarn workspaces, and webpack did not automatically handle `node_modules` being outside the workspace where `serverless create ` was run. To fix, the externals line in webpack.config.js is changed from
+The initial setup for this project ran `serverless create` in the root directory, which created and deployed successfully. We decided to use yarn workspaces, and webpack did not automatically handle `node_modules` being outside the workspace where `serverless create` was run. To fix, the externals line in webpack.config.js is changed from
 
     externals: [nodeExternals()],
 
--- workspaces/serverless-wrappers/handler.ts
+++ workspaces/serverless-wrappers/handler.ts
@@ -1,13 +1,37 @@
 import { Handler, ScheduledEvent, Context } from 'aws-lambda';
 import 'source-map-support/register';
 
-// in aws this lambda is named bball-slackbot-upgraded-[dev/prod]-runBot
+// need aws-sdk to invoke lambdas
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda({ region: 'us-east-1' });
+
+interface InvokeLambdaParams {
+  FunctionName: string,
+  InvocationType: string,
+  LogType: string,
+  Payload: string
+}
+
+// in aws this lambda is named bball-slackbot-upgraded-[dev/test/prod]-runBot
 export const runBot: Handler = async (event: ScheduledEvent, _context: Context) => {
-  console.log(`in runBot`);
-  console.log(`Event: \n${JSON.stringify(event, null, 2)}`);
+  const mlbLambdaParams: InvokeLambdaParams = {
+    FunctionName: 'bball-slackbot-upgraded-dev-checkMLBGamesLambda',
+    InvocationType: 'RequestResponse',
+    LogType: 'Tail',
+    Payload: JSON.stringify({ test: 'test' })
+  }
+
+  try {
+    const mlbResponse = await lambda.invoke(mlbLambdaParams).promise();
+    console.log(`mlbResponse: `, mlbResponse);
+  } catch (error) {
+    console.error(error);
+  }
+
   return _context.logStreamName;
 }
 
+// in aws this lambda is named bball-slackbot-upgraded-[dev/test/prod]-checkMLBGamesLambda
 export const checkMLBGamesLambda: Handler = async (event, _context: Context) => {
   console.log(`in checkMLBGamesLambda`);
   console.log(`Event: \n${JSON.stringify(event, null, 2)}`);
